18e1a60614106d3a5ea601a140844b7efc498351
Andrew Henry <andrew.henry@pitt.edu>
2020-10-25 21:25:11 -0400

    Add export for runtime generation with interpolation

--- src/getStyles.js
+++ src/getStyles.js
@@ -107,3 +107,85 @@ export default (classes, t, state) => {
 
   return astify(isEmpty(styles) ? {} : styles, t)
 }
+
+export const cssify = (classes, t, state = {}) => {
+  // Strip pipe dividers " | "
+  classes = classes.replace(/ \| /g, ' ')
+
+  // Unwrap grouped variants
+  classes = handleVariantGroups(classes)
+
+  // Move and sort the responsive items to the end of the list
+  const classesOrdered = orderByScreens(classes, state)
+
+  const theme = getTheme(state.config.theme)
+
+  // Merge styles into a single css object
+  const styles = classesOrdered.reduce((results, classNameRaw) => {
+    doPrechecks([precheckGroup], { classNameRaw })
+
+    const pieces = getPieces({ classNameRaw, state })
+    const { className, hasVariants } = pieces
+
+    assert(!className, () =>
+      hasVariants ? logNotFoundVariant({ classNameRaw }) : logNotFoundClass
+    )
+
+    const {
+      hasMatches,
+      hasUserPlugins,
+      dynamicKey,
+      dynamicConfig,
+      corePlugin,
+      type,
+    } = getProperties(className, state)
+
+    // Kick off suggestions when no class matches
+    assert(!hasMatches && !hasUserPlugins, () =>
+      errorSuggestions({ pieces, state })
+    )
+
+    const styleHandler = {
+      static: () => handleStatic({ pieces }),
+      dynamic: () =>
+        handleDynamic({ theme, pieces, state, dynamicKey, dynamicConfig }),
+      userPlugin: () => handleUserPlugins({ state, className }),
+      corePlugin: () =>
+        handleCorePlugins({
+          theme,
+          pieces,
+          state,
+          corePlugin,
+          classNameRaw,
+          dynamicKey,
+        }),
+    }
+
+    let style
+
+    if (hasUserPlugins) {
+      style = applyTransforms({
+        type,
+        pieces,
+        style: styleHandler.userPlugin(),
+      })
+    }
+
+    // Check again there are no userPlugin matches
+    assert(!hasMatches && !style, () => errorSuggestions({ pieces, state }))
+
+    style =
+      style || applyTransforms({ type, pieces, style: styleHandler[type]() })
+
+    const result = deepMerge(
+      results,
+      pieces.hasVariants ? addVariants({ results, style, pieces }) : style
+    )
+
+    state.debug && debug(classNameRaw, style)
+
+    return result
+  }, {})
+
+  return styles
+}
--- src/macro.js
+++ src/macro.js
@@ -24,6 +24,7 @@ import { handleGlobalStylesFunction } from './macro/GlobalStyles'
 import { handleTwProperty, handleTwFunction } from './macro/tw'
 import getUserPluginData from './utils/getUserPluginData'
 import { debugPlugins } from './logging'
+import { cssify } from './getStyles'
 
 const getPackageUsed = ({ config: { preset }, cssImport, styledImport }) => ({
   isEmotion: preset === 'emotion' || cssImport.from.includes('emotion'),
@@ -154,3 +155,65 @@ const twinMacro = ({ babel: { types: t }, references, state, config }) => {
 }
 
 export default createMacro(twinMacro, { configName: 'twin' })
+
+export const twr = className =>
+  ((
+    className,
+    state = { file: { opts: { sourceRoot: '.' } }, debug: true },
+    config = { debug: true },
+    t = {}
+  ) => {
+    const { configExists, tailwindConfig } = getConfigProperties(state, config)
+
+    state.configExists = configExists
+    state.config = tailwindConfig
+    state.hasSuggestions =
+      typeof config.hasSuggestions === 'undefined'
+        ? true
+        : Boolean(config.hasSuggestions)
+    state.allowStyleProp =
+      typeof config.allowStyleProp === 'undefined'
+        ? false
+        : Boolean(config.allowStyleProp)
+
+    /* eslint-disable-next-line unicorn/prevent-abbreviations */
+    const isDev =
+      process.env.NODE_ENV === 'development' ||
+      process.env.NODE_ENV === 'dev' ||
+      false
+    state.isDev = isDev
+    state.isProd = !isDev
+
+    state.debugProp = isDev ? Boolean(config.debugProp) : false
+    state.debug = isDev ? Boolean(config.debug) : false
+
+    state.userPluginData = getUserPluginData({ config: state.config })
+    isDev &&
+      Boolean(config.debugPlugins) &&
+      state.userPluginData &&
+      debugPlugins(state.userPluginData)
+
+    // Styled import
+    const styledImport = getStyledConfig(config)
+    state.styledImport = styledImport
+
+    // Sassy pseudo prefix (eg: the & in &:hover)
+    state.sassyPseudo =
+      config.sassyPseudo !== undefined
+        ? config.sassyPseudo === true
+        : state.isGoober
+
+    if (state.styledIdentifier === null) {
+      state.styledIdentifier = 'uuid3'
+    } else {
+      state.existingStyledIdentifier = true
+    }
+
+    if (state.cssIdentifier === null) {
+      state.cssIdentifier = 'uuid4'
+    } else {
+      state.existingCssIdentifier = true
+    }
+
+    return cssify(className, t, state)
+  })(className)
